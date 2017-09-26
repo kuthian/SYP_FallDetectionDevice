544293163a6452dc4719eefef2c220471c11840c
kuthian <31873544+kuthian@users.noreply.github.com>
2017-09-26 19:47:51 -0300

    New App name

---
+++ FallCompanionApp/app/src/main/java/com/example/fallcompanion/FallData.java
@@ -0,0 +1,13 @@
+package com.example.fallcompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FallData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fall_data);
+    }
+}
